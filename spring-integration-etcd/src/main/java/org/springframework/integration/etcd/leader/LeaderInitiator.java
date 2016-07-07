f3707a11f17ec2509f745ad8789133a9ce74a345
Artem Bilan <abilan@pivotal.io>
2016-07-06 21:01:19 -0400

    Polishing
    * Upgrade Gradle 2.14
    * Upgrade dependencies
    * Add `log4j.properties`
    * Make `LeaderInitiator` as `SmartLifecycle`
    * Fix compatibility with the latest `etcd4j`

--- spring-integration-etcd/src/main/java/org/springframework/integration/etcd/leader/LeaderInitiator.java
+++ spring-integration-etcd/src/main/java/org/springframework/integration/etcd/leader/LeaderInitiator.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2014-2015 the original author or authors.
+ * Copyright 2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,38 +23,38 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
-import org.springframework.context.Lifecycle;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.integration.leader.Candidate;
 import org.springframework.integration.leader.Context;
 import org.springframework.integration.leader.DefaultCandidate;
 import org.springframework.integration.leader.event.DefaultLeaderEventPublisher;
 import org.springframework.integration.leader.event.LeaderEventPublisher;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 import org.springframework.util.Assert;
 
 import mousio.etcd4j.EtcdClient;
+import mousio.etcd4j.responses.EtcdAuthenticationException;
 import mousio.etcd4j.responses.EtcdException;
 
 /**
- * Bootstrap leadership {@link Candidate candidates}
- * with etcd. Upon construction, {@link #start} must be invoked to
- * register the candidate for leadership election.
+ * Bootstrap leadership {@link Candidate candidates} with etcd.
  *
  * @author Venil Noronha
  * @author Patrick Peralta
+ * @author Artem Bilan
  */
-public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableBean, ApplicationEventPublisherAware {
+public class LeaderInitiator implements SmartLifecycle, DisposableBean, ApplicationEventPublisherAware {
 
-	private static final Logger logger = LoggerFactory.getLogger(LeaderInitiator.class);
+	private static final Log logger = LogFactory.getLog(LeaderInitiator.class);
 
 	/**
 	 * TTL for etcd entry in seconds.
@@ -72,6 +72,23 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	private final static String DEFAULT_NAMESPACE = "spring-cloud";
 
 	/**
+	 * Executor service for running leadership daemon.
+	 */
+	private final ExecutorService leaderExecutorService =
+			Executors.newSingleThreadExecutor(new DaemonCustomizableThreadFactory("Etcd-Leadership"));
+
+	/**
+	 * Executor service for running leadership worker daemon.
+	 */
+	private final ExecutorService workerExecutorService =
+			Executors.newSingleThreadExecutor(new DaemonCustomizableThreadFactory("Etcd-Leadership-Worker"));
+
+	/**
+	 * The {@link EtcdContext} instance.
+	 */
+	private final EtcdContext context = new EtcdContext();
+
+	/**
 	 * {@link EtcdClient} instance.
 	 */
 	private final EtcdClient client;
@@ -82,28 +99,9 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	private final Candidate candidate;
 
 	/**
-	 * Executor service for running leadership daemon.
+	 * The base etcd path where candidate id is to be stored.
 	 */
-	private final ExecutorService leaderExecutorService = Executors.newSingleThreadExecutor(new ThreadFactory() {
-		@Override
-		public Thread newThread(Runnable r) {
-			Thread thread = new Thread(r, "Etcd-Leadership");
-			thread.setDaemon(true);
-			return thread;
-		}
-	});
-
-	/**
-	 * Executor service for running leadership worker daemon.
-	 */
-	private final ExecutorService workerExecutorService = Executors.newSingleThreadExecutor(new ThreadFactory() {
-		@Override
-		public Thread newThread(Runnable r) {
-			Thread thread = new Thread(r, "Etcd-Leadership-Worker");
-			thread.setDaemon(true);
-			return thread;
-		}
-	});
+	private final String baseEtcdPath;
 
 	/**
 	 * Flag that indicates whether the current candidate is
@@ -130,12 +128,6 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	private volatile Future<Void> workerFuture;
 
 	/**
-	 * Flag that indicates whether the leadership election for
-	 * this {@link #candidate} is running.
-	 */
-	private volatile boolean running;
-
-	/**
 	 * Leader event publisher.
 	 */
 	private volatile LeaderEventPublisher leaderEventPublisher = new DefaultLeaderEventPublisher();
@@ -143,14 +135,21 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	private boolean customPublisher = false;
 
 	/**
-	 * The {@link EtcdContext} instance.
+	 * @see SmartLifecycle
 	 */
-	private final EtcdContext context;
+	private volatile boolean autoStartup = true;
 
 	/**
-	 * The base etcd path where candidate id is to be stored.
+	 * @see SmartLifecycle which is an extension of org.springframework.context.Phased
 	 */
-	private final String baseEtcdPath;
+	private volatile int phase;
+
+	/**
+	 * Flag that indicates whether the leadership election for
+	 * this {@link #candidate} is running.
+	 */
+	private volatile boolean running;
+
 
 	/**
 	 * Construct a {@link LeaderInitiator}.
@@ -170,10 +169,22 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	 * @param namespace	 Etcd namespace
 	 */
 	public LeaderInitiator(EtcdClient client, Candidate candidate, String namespace) {
+		Assert.notNull(client, "'client' must not be null");
+		Assert.notNull(candidate, "'candidate' must not be null");
 		this.client = client;
 		this.candidate = candidate;
-		this.context = new EtcdContext();
 		this.baseEtcdPath = (namespace == null ? DEFAULT_NAMESPACE : namespace) + "/" + candidate.getRole();
+	}
+
+	/**
+	 * Sets the {@link LeaderEventPublisher}.
+	 *
+	 * @param leaderEventPublisher the event publisher
+	 */
+	public void setLeaderEventPublisher(LeaderEventPublisher leaderEventPublisher) {
+		Assert.notNull(leaderEventPublisher);
+		this.leaderEventPublisher = leaderEventPublisher;
+		this.customPublisher = true;
 	}
 
 	@Override
@@ -181,6 +192,25 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 		if (!this.customPublisher) {
 			this.leaderEventPublisher = new DefaultLeaderEventPublisher(applicationEventPublisher);
 		}
+	}
+
+	public void setAutoStartup(boolean autoStartup) {
+		this.autoStartup = autoStartup;
+	}
+
+	@Override
+	public boolean isAutoStartup() {
+		return this.autoStartup;
+	}
+
+	public void setPhase(int phase) {
+		this.phase = phase;
+	}
+
+
+	@Override
+	public int getPhase() {
+		return this.phase;
 	}
 
 	/**
@@ -191,6 +221,14 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 		if (!this.running) {
 			this.running = true;
 			this.initiatorFuture = this.leaderExecutorService.submit(new Initiator());
+		}
+	}
+
+	@Override
+	public void stop(Runnable callback) {
+		stop();
+		if (callback != null) {
+			callback.run();
 		}
 	}
 
@@ -215,27 +253,10 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	}
 
 	@Override
-	public void afterPropertiesSet() throws Exception {
-		start();
-	}
-
-	@Override
 	public void destroy() throws Exception {
 		stop();
 		this.workerExecutorService.shutdown();
 		this.leaderExecutorService.shutdown();
-		this.workerExecutorService.awaitTermination(30, TimeUnit.SECONDS);
-		this.leaderExecutorService.awaitTermination(30, TimeUnit.SECONDS);
-	}
-
-	/**
-	 * Sets the {@link LeaderEventPublisher}.
-	 *
-	 * @param leaderEventPublisher the event publisher
-	 */
-	public void setLeaderEventPublisher(LeaderEventPublisher leaderEventPublisher) {
-		Assert.notNull(leaderEventPublisher);
-		this.leaderEventPublisher = leaderEventPublisher;
 	}
 
 	/**
@@ -274,9 +295,12 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	/**
 	 * Tries to delete the candidate's entry from etcd.
 	 */
-	private void tryDeleteCandidateEntry() {
+	private void tryDeleteCandidateEntry() throws EtcdAuthenticationException {
 		try {
-			this.client.delete(this.baseEtcdPath).prevValue(this.candidate.getId()).send().get();
+			this.client.delete(this.baseEtcdPath)
+					.prevValue(this.candidate.getId())
+					.send()
+					.get();
 		}
 		catch (EtcdException e) {
 			logger.warn("Couldn't delete candidate's entry from etcd", e);
@@ -313,7 +337,7 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 	class Initiator implements Callable<Void> {
 
 		@Override
-		public Void call() throws InterruptedException {
+		public Void call() throws Exception {
 			try {
 				while (LeaderInitiator.this.running) {
 					if (LeaderInitiator.this.relinquishLeadership) {
@@ -345,7 +369,7 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 		 * @throws InterruptedException	if the current thread was interrupted
 		 * while notifying revocation.
 		 */
-		private void relinquishLeadership() throws InterruptedException {
+		private void relinquishLeadership() throws Exception {
 			tryDeleteCandidateEntry();
 			notifyRevoked();
 		}
@@ -360,9 +384,13 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 		 * @throws InterruptedException	if the current thread was interrupted
 		 * while notifying revocation.
 		 */
-		private void sendHeartBeat() throws InterruptedException {
+		private void sendHeartBeat() throws Exception {
 			try {
-				LeaderInitiator.this.client.put(LeaderInitiator.this.baseEtcdPath, LeaderInitiator.this.candidate.getId()).ttl(TTL).prevValue(LeaderInitiator.this.candidate.getId()).send().get();
+				LeaderInitiator.this.client.put(LeaderInitiator.this.baseEtcdPath, LeaderInitiator.this.candidate.getId())
+						.ttl(TTL)
+						.prevValue(LeaderInitiator.this.candidate.getId())
+						.send()
+						.get();
 			}
 			catch (EtcdException e) {
 				notifyRevoked();
@@ -378,9 +406,14 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 		 * Tries to acquire leadership by posting the candidate's id to etcd. If the etcd call
 		 * is successful, it is assumed that the current candidate is now leader.
 		 */
-		private void tryAcquire() {
+		private void tryAcquire() throws EtcdAuthenticationException {
 			try {
-				LeaderInitiator.this.client.put(LeaderInitiator.this.baseEtcdPath, LeaderInitiator.this.candidate.getId()).ttl(TTL).prevExist(false).send().get();
+				LeaderInitiator.this.client.put(LeaderInitiator.this.baseEtcdPath,
+						LeaderInitiator.this.candidate.getId())
+						.ttl(TTL)
+						.prevExist(false)
+						.send()
+						.get();
 				notifyGranted();
 			}
 			catch (EtcdException e) {
@@ -413,8 +446,18 @@ public class LeaderInitiator implements Lifecycle, InitializingBean, DisposableB
 
 		@Override
 		public String toString() {
-			return String.format("EtcdContext{role=%s, id=%s, isLeader=%s}",
-					LeaderInitiator.this.candidate.getRole(), LeaderInitiator.this.candidate.getId(), isLeader());
+			return "EtcdContext{role=" + LeaderInitiator.this.candidate.getRole() +
+					", id=" + LeaderInitiator.this.candidate.getId() +
+					", isLeader=" + isLeader() + "}";
+		}
+
+	}
+
+	private static final class DaemonCustomizableThreadFactory extends CustomizableThreadFactory {
+
+		DaemonCustomizableThreadFactory(String threadNamePrefix) {
+			super(threadNamePrefix);
+			setDaemon(true);
 		}
 
 	}
---
+++ spring-integration-etcd/src/main/java/org/springframework/integration/etcd/leader/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * ETCD-based Spring Integration leader support classes.
+ */
+package org.springframework.integration.etcd.leader;
